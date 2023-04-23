59bf58232e53148a26d85cfa8782dead7b52e86d
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-23 18:07:32 +0300

    added header style

--- src/components/Starships.js
+++ src/components/Starships.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import logo from './image/starwars_logo.png'
 function Starships() {
   return (
-    <div>Starships</div>
+    <div className="container text-center pt-5">
+    <img width={300} src={logo} alt="star wars" />
+    <div className="row justify-content-center mt-5">
+      <div className="col-6">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Name or Model"
+        />
+      </div>
+      <div className="col-auto">
+        <button type="submit" className="btn btn-primary">
+          Search
+        </button>
+      </div>
+    </div>
+  </div>
   )
 }
 
