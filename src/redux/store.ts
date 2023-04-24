bf9d53954b74a0ed02cf4a1364180e584de845ad
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-24 15:47:15 +0300

    added redux toolkit

---
+++ src/redux/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+import  starShipsReducer from './starShipsSlice'
+import starShipDetailReducer from './starShipDetailSlice'
+
+export const store = configureStore({
+  reducer: {
+    starships: starShipsReducer,
+    starshipDetail: starShipDetailReducer,
+   
+  },
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
