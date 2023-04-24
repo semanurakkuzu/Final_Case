6081dff960e906e6bca2e02ca75d9bb9ab9b42d1
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-24 15:15:58 +0300

    coverted to typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Starships from "./components/Starship";
+function App() {
+  return (
+   <div>
+     <Starships></Starships>
+   </div>
+  );
+}
+
+export default App;
--- src/components/StarshipDetail.tsx
+++ src/components/StarshipDetail.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
-
 function StarshipDetail() {
   return (
     <div className="theme-dark">
       <div className="container pt-5">
-        <div className="card mx-auto detailCard py-2 border border-5">
+        <div className="card mx-auto detailCard py-2">
           <div className="ms-3">
             <button className="btn fs-3">
-              <FontAwesomeIcon icon={faArrowLeftLong} />
+            <FontAwesomeIcon icon={faArrowLeftLong} />
             </button>
           </div>
           <div className="card-body text-center">
@@ -20,44 +19,44 @@ function StarshipDetail() {
               
             />
             <p className="card-title fs-1 text-center my-4">Star Destroyer</p>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Model: </span>
                   <span>CR-90 corvette</span>
                 </div>
               </li>
-              <li class="list-group-item">
+              <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Hyperdrive Rating: </span>
                   <span>2.0</span>
                 </div>
               </li>
-              <li class="list-group-item">
+              <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Passengers: </span>
                   <span>75</span>
                 </div>
               </li>
-              <li class="list-group-item">
+              <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Max Atmospering Speed: </span>
                   <span>1000</span>
                 </div>
               </li>
-              <li class="list-group-item">
+              <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Manufacturer: </span>
                   <span>Sinear Fleet Systems, Cyngus Spaceworks</span>
                 </div>
               </li>
-              <li class="list-group-item">
+              <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Crew: </span>
                   <span>5</span>
                 </div>
               </li>
-              <li class="list-group-item">
+              <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Cargo Capacity: </span>
                   <span>180000</span>
