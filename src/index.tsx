42e10a44fdf78892f43b70673042f8f1c16062f1
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-24 17:49:45 +0300

    added react query

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { QueryClient, QueryClientProvider} from 'react-query'
 
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+    </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
