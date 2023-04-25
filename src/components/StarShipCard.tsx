847258762fa4f1e11a7f0ff0386a74395b528a55
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-25 12:07:25 +0300

    added startship card

---
+++ src/components/StarShipCard.tsx
@@ -0,0 +1,32 @@
+import { StarShip } from '../types/Starship'
+
+type StarShipCardProps = {
+  starShip: StarShip
+}
+
+function StarShipCard({starShip}: StarShipCardProps) {
+  return (
+    <div className="card">
+    <img
+      src="https://www.denofgeek.com/wp-content/uploads/2019/12/x-wing.jpg?w=1024"
+      className="rounded card-img-top cardImg"
+      alt="starship"
+    />
+    <div className="card-body">
+      <p className="card-title fs-2 fw-bolder text-center my-3 ">
+        {starShip.name}
+      </p>
+      <div className="card-text fs-5">
+        <span className="fw-semibold">Model: </span>
+        <span>{starShip.model}</span>
+      </div>
+      <div className="card-text fs-5">
+        <span className="fw-semibold">Hyperdrive Rating: </span>
+        <span>{starShip.hyperdrive_rating}</span>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default StarShipCard
