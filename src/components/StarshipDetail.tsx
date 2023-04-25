4c58a0c814cfd1c61399c2d00b2b209d9822dad2
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-25 17:58:16 +0300

    added ship to redux and taked ship data from redux

--- src/components/StarshipDetail.tsx
+++ src/components/StarshipDetail.tsx
@@ -2,23 +2,31 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
+import { useAppDispatch } from '../redux/hooks';
+import { useAppSelector } from '../redux/hooks';
+import { setStarShip } from '../redux/starShipDetailSlice';
 import { useParams } from "react-router";
 import { useQuery } from 'react-query';
 import { fetchShip } from '../services/starshipService';
 
 function StarShipDetail() {
-
 const {id} = useParams()
 
-const {data} = useQuery(['ships', id], () => 
+const dispatch = useAppDispatch();
+
+const ship = useQuery(['ship', id], () => 
 fetchShip(id))
 
+if (ship.status === 'success') {
+  dispatch(setStarShip(ship.data));
+}
 
+const starShip = useAppSelector((state) => state.starshipDetail.starShip);
 
   return (
     <div className="theme-dark">
       <div className="container pt-5">
-       {data &&
+       {starShip &&
        <div className="card mx-auto detailCard py-2">
           <div className="ms-3">
             <Link to='/' className="color-dark fs-3">
@@ -32,48 +40,48 @@ fetchShip(id))
               alt="starship"
               
             />
-            <p className="card-title fs-1 text-center my-4">{data.name}</p>
+            <p className="card-title fs-1 text-center my-4">{starShip.name}</p>
             <ul className="list-group list-group-flush">
               <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Model: </span>
-                  <span>{data.model}</span>
+                  <span>{starShip.model}</span>
                 </div>
               </li>
               <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Hyperdrive Rating: </span>
-                  <span>{data.hyperdrive_rating}</span>
+                  <span>{starShip.hyperdrive_rating}</span>
                 </div>
               </li>
               <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Passengers: </span>
-                  <span>{data.passengers}</span>
+                  <span>{starShip.passengers}</span>
                 </div>
               </li>
               <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Max Atmospering Speed: </span>
-                  <span>{data.max_atmosphering_speed}</span>
+                  <span>{starShip.max_atmosphering_speed}</span>
                 </div>
               </li>
               <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Manufacturer: </span>
-                  <span>{data.manufacturer}</span>
+                  <span>{starShip.manufacturer}</span>
                 </div>
               </li>
               <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Crew: </span>
-                  <span>{data.crew}</span>
+                  <span>{starShip.crew}</span>
                 </div>
               </li>
               <li className="list-group-item">
                 <div className="card-text fs-4">
                   <span className="fw-semibold">Cargo Capacity: </span>
-                  <span>{data.cargo_capacity}</span>
+                  <span>{starShip.cargo_capacity}</span>
                 </div>
               </li>
             </ul>
