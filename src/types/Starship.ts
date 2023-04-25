b20d590538f647856a8caadfeefffaae11ce1fba
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-25 12:09:23 +0300

    added startship data type

---
+++ src/types/Starship.ts
@@ -0,0 +1,20 @@
+export interface StarShip {
+    name: String
+    model: String
+    manufacturer: String
+    cost_in_credits: String
+    length: String
+    max_atmosphering_speed: String
+    crew: String
+    passengers: String
+    cargo_capacity: String
+    consumables: String
+    hyperdrive_rating: String
+    MGLT: String
+    starship_class: String
+    pilots: Array<String> 
+    films: Array<String> 
+    created: String
+    edited: String
+    url: String
+  }
