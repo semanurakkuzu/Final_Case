9e566222e7bc25c935c7529d0586c73a63951487
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-25 12:08:44 +0300

    added fetch starship search

--- src/services/starshipService.ts
+++ src/services/starshipService.ts
@@ -1,6 +1,15 @@
 import axios from "axios"
+
+const starShipEndPoint = 'https://swapi.dev/api/starships/'
+
 export const fetchStarShipList = async() => {
-    const {data} = await axios.get('')
+    const {data} = await axios.get(starShipEndPoint)
+
+    return data;
+}
+
+export const fetchStarShipSearch = async(searchInputValue: string) => {
+    const { data } = await axios.get(`${starShipEndPoint}?search=${searchInputValue}`)
 
     return data;
 }
