ecb0a30b96fc6d6504237e89cfeb50864cbb953e
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-25 12:07:00 +0300

    edited starships

--- src/components/StarShips.tsx
+++ src/components/StarShips.tsx
@@ -1,62 +1,26 @@
-import React from 'react'
-import { useQuery } from 'react-query'
-import logo from '../image/starwars_logo.png'
+import { useAppSelector } from '../redux/hooks';
+import { StarShip } from '../types/Starship';
+import StarShipSearch from './StarShipSearch';
+import StarShipCard from './StarShipCard';
 
 export default function StarShips() {
-  const query = useQuery('repoData', () =>
-    fetch('https://api.github.com/repos/tannerlinsley/react-query')
-      .then((res) => res.json())
-  )
-
-  console.log(query.data)
+  const starShips = useAppSelector((state) => state.starships.starShipData);
 
   return (
     <>
       <div className="theme-dark">
         <div className="container-fluid">
-          <div className="container text-center pt-5">
-            <img width={300} src={logo} alt="star wars" />
-            <div className="row justify-content-center mt-5">
-              <div className="col-6">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Name or Model"
-                />
-              </div>
-              <div className="col-auto">
-                <button type="submit" className="btn btn-primary">
-                  Search
-                </button>
-              </div>
-            </div>
-          </div>
+          <StarShipSearch></StarShipSearch>
           <div className="row justify-content-center mt-5">
-            <div className="col-auto mb-4">
-              <div className="card">
-                <img
-                  src="https://www.denofgeek.com/wp-content/uploads/2019/12/x-wing.jpg?w=1024"
-                  className="rounded card-img-top cardImg"
-                  alt="starship"
-                />
-                <div className="card-body">
-                  <p className="card-title fs-2 fw-bolder text-center my-3 ">
-                    Star Destroyer
-                  </p>
-                  <div className="card-text fs-5">
-                    <span className="fw-semibold">Model: </span>
-                    <span>CR-90 corvette</span>
-                  </div>
-                  <div className="card-text fs-5">
-                    <span className="fw-semibold">Hyperdrive Rating: </span>
-                    <span>2.0</span>
-                  </div>
+            {starShips &&
+              starShips.map((starShip: StarShip, key: number) => (
+                <div key={key} className="col-auto mb-4">
+                  <StarShipCard starShip={starShip} />
                 </div>
-              </div>
-            </div>
+              ))}
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
