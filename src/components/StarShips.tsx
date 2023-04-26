1c10a4c4ba94ee687f714367dbf748a165730b7d
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-26 18:01:44 +0300

    added infiniteQuery and starShipSearch to Starship

--- src/components/StarShips.tsx
+++ src/components/StarShips.tsx
@@ -1,24 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import logo from '../image/starwars_logo.png';
 import { useAppSelector } from '../redux/hooks';
+import { useAppDispatch } from '../redux/hooks';
+import { setStarShips } from '../redux/starShipsSlice';
 import { StarShip } from '../types/Starship';
-import StarShipSearch from './StarShipSearch';
+import { useInfiniteQuery } from 'react-query';
+import { fetchStarShipList } from '../services/starshipService';
 import StarShipCard from './StarShipCard';
+import { useDebounce } from 'use-debounce';
 
 export default function StarShips() {
+  const [searchInputValue, setSearchInputValue] = useState<string>('');
+
+  const dispatch = useAppDispatch();
+  const [debouncedSearchInputValue] = useDebounce<string>(searchInputValue, 500);
+
+  const {
+    data,
+
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status
+  } = useInfiniteQuery(
+    ['ships', debouncedSearchInputValue],
+
+    ({ pageParam }) => fetchStarShipList(debouncedSearchInputValue, pageParam),
+    {
+      getNextPageParam: (lastPages, pages) => {
+        if ( lastPages.next === null) {
+          return;
+        }
+
+        return pages.length + 1;
+      }
+    }
+  );
+  console.log(data)
+
+  useEffect(() => {
+    if (status === 'success') {
+      dispatch(setStarShips(data));
+    }
+  }, [data, dispatch, status]);
+
+  function handleSearchInputChange(event: React.SyntheticEvent<EventTarget>) {
+    setSearchInputValue((event.target as HTMLInputElement).value);
+  }
+
   const starShips = useAppSelector((state) => state.starships.starShipData);
 
   return (
     <>
       <div className="theme-dark">
         <div className="container-fluid">
-          <StarShipSearch></StarShipSearch>
+          <div className="container text-center pt-5">
+            <img width={300} src={logo} alt="star wars" />
+            <div className="row justify-content-center mt-5">
+              <div className="col-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter name or model of starship.."
+                  value={searchInputValue}
+                  onChange={handleSearchInputChange}
+                />
+              </div>
+            </div>
+          </div>
           <div className="row justify-content-center mt-5">
             {starShips &&
-              starShips.map((starShip: StarShip, key: number) => (
-                <div key={key} className="col-auto mb-4">
-                  <StarShipCard starShip={starShip} />
-                </div>
+              starShips.pages?.map((group, i) => (
+                <React.Fragment key={i}>
+                  {group.results?.map((starShip: StarShip, i: number) => (
+                    <div className="col-auto mb-4" key={i}>
+                      <StarShipCard starShip={starShip} />
+                    </div>
+                  ))}
+                </React.Fragment>
               ))}
           </div>
+          <div className="text-center">
+            <button onClick={() => fetchNextPage()} disabled={!hasNextPage || isFetchingNextPage}>
+              {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load More' : 'Nothing more to load'}
+            </button>
+          </div>
+          <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
         </div>
       </div>
     </>
