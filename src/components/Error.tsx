fb9002d63d13a3b228ee5edec638223972bb89bb
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-27 19:14:45 +0300

    added  error component design

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 function Error() {
   return (
-    <div>Error</div>
-  )
+    <div className="theme-dark row text-center align-items-center">
+      <div>
+       <p className='text-color fs-1 fw-bolder'>404 Error</p> 
+        <p className='text-color fs-5'>The page you are looking for does not exist.The page you are looking for does not exist.</p>
+      </div>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
